460973865e0844744f6fde6614d236b83b34fa3a
junhyeokhi <godjh1112@gmail.com>
2026-04-09 15:28:55 +0900

    20260409_search

--- pybo/views/answer_views.py
+++ pybo/views/answer_views.py
@@ -1,14 +1,16 @@
 from datetime import datetime
 
-from flask import Blueprint, request, redirect, url_for, render_template
+from flask import Blueprint, request, redirect, url_for, render_template, g, flash
 
 from pybo import db
 from pybo.forms import AnswerForm
 from pybo.models import Question, Answer
+from pybo.views.auth_views import login_required
 
 bp = Blueprint('answer', __name__, url_prefix='/answer')
 
 @bp.route('/create/<int:question_id>', methods=['POST'])
+@login_required
 def create(question_id):
     form = AnswerForm()
     # 답변 등록할 질문을 DB 조회
@@ -18,10 +20,39 @@ def create(question_id):
         # content = request.form.get('content')
         content = form.content.data
         # Answer객체를 생성
-        answer = Answer(content=content, create_date=datetime.now())
+        answer = Answer(content=content, create_date=datetime.now(), user=g.user)
         # 해당 질문에 답변 등록
         question.answer_set.append(answer)
         db.session.commit()
         return redirect(url_for('question.detail', question_id=question_id))
 
     return render_template('question/question_detail.html', question=question, form=form)
+
+@bp.route('/modify/<int:answer_id>', methods=['GET', 'POST'])
+@login_required
+def modify(answer_id):
+    answer = Answer.query.get_or_404(answer_id)
+    if g.user != answer.user:
+        flash('수정 권한이 없습니다.')
+        return redirect(url_for('question.detail', question_id=answer.question_id))
+    if request.method == 'POST':
+        form = AnswerForm()
+        if form.validate_on_submit():
+            form.populate_obj(answer)
+            answer.modify_date = datetime.now()
+            db.session.commit()
+            return redirect(url_for('question.detail', question_id=answer.question_id))
+    else:
+        form = AnswerForm(obj=answer)
+        return render_template('answer/answer_form.html', form=form)
+
+@bp.route('/delete/<int:answer_id>')
+@login_required
+def delete(answer_id):
+    answer = Answer.query.get_or_404(answer_id)
+    if g.user != answer.user:
+        flash('삭제 권한이 없습니다.')
+    else:
+        db.session.delete(answer)
+        db.session.commit()
+    return redirect(url_for('question.detail', question_id=answer.question_id))
--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -1,19 +1,40 @@
 from datetime import datetime
 
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, g, flash
 
 from pybo import db
 from pybo.forms import QuestionForm, AnswerForm
-from pybo.models import Question
+from pybo.models import Question, Answer, User
+from pybo.views.auth_views import login_required
 
 bp = Blueprint('question', __name__, url_prefix='/question')
 
+
 @bp.route('/list/')
 def _list():
-    page = request.args.get('page', 1, type=int) # 페이지 쿼리 스트링값 가져오기
+    page = request.args.get('page', 1, type=int)
+    kw = request.args.get('kw', '', type=str)
     question_list = Question.query.order_by(Question.create_date.desc())
-    question_list = question_list.paginate(page=page, per_page=10) # 한 페이지에 보여야할 개수
-    return render_template('question/question_list.html', question_list=question_list)
+
+    if kw:
+        search = '%%{}%%'.format(kw)
+        # 1. 서브쿼리 작성 (답변과 답변자 정보를 미리 모음)
+        sub_query = db.session.query(Answer.question_id, Answer.content, User.username) \
+            .join(User, Answer.user_id == User.id).subquery()
+
+        # 2. 메인 쿼리에 조인 및 필터 적용
+        question_list = question_list.join(User) \
+            .outerjoin(sub_query, sub_query.c.question_id == Question.id) \
+            .filter(
+            Question.subject.ilike(search) |  # 질문 제목
+            Question.content.ilike(search) |  # 질문 내용
+            User.username.ilike(search) |  # 질문 작성자
+            sub_query.c.content.ilike(search) |  # 답변 내용
+            sub_query.c.username.ilike(search)  # 답변 작성자
+        ).distinct()
+
+    question_list = question_list.paginate(page=page, per_page=10)
+    return render_template('question/question_list.html', question_list=question_list, page=page, kw=kw)
 
 @bp.route('/detail/<int:question_id>/')
 def detail(question_id):
@@ -22,11 +43,42 @@ def detail(question_id):
     return render_template('question/question_detail.html', question=question, form=form)
 
 @bp.route('/create/', methods=['GET', 'POST'])
+@login_required
 def create():
     form = QuestionForm()
     if request.method == 'POST' and form.validate_on_submit():
-        question = Question(subject=form.subject.data, content=form.content.data, create_date=datetime.now())
+        question = Question(subject=form.subject.data, content=form.content.data, create_date=datetime.now(), user=g.user.id)
         db.session.add(question)
         db.session.commit()
         return redirect(url_for('main.index'))
     return render_template('question/question_form.html', form=form)
+
+@bp.route('/modify/<int:question_id>/', methods=['GET', 'POST'])
+@login_required
+def modify(question_id):
+    question = Question.query.get_or_404(question_id)
+    if g.user != question.user:
+        flash('수정 권한이 없습니다')
+        return redirect(url_for('question.detail', question_id=question_id))
+
+    if request.method == 'POST':
+        form = QuestionForm()
+        if form.validate_on_submit():
+            form.populate_obj(question)
+            question.modify_date=datetime.now() # 수정일시 저장
+            db.session.commit()
+            return redirect(url_for('question.detail', question_id=question.id))
+    else:
+        form = QuestionForm(obj=question)
+    return render_template('question/question_form.html', form=form)
+
+@bp.route('/delete/<int:question_id>/')
+@login_required
+def delete(question_id):
+    question = Question.query.get_or_404(question_id)
+    if g.user != question.user:
+        flash('삭제 권한이 없습니다.')
+        return redirect(url_for('question.detail', question_id=question_id))
+    db.session.delete(question)
+    db.session.commit()
+    return redirect(url_for('question._list'))
